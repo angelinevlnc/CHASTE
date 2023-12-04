ca0581d0ac3eee95347dfe42dc9269bc62270599
Gabrielle Antoinette <gabrielle.a21@mhs.istts.ac.id>
2023-12-04 23:30:43 +0700

    revisi

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -19,35 +19,25 @@ class MenuController extends Controller
         'harga' => 'required',
         'kategori' => 'required|max:255',
         'deskripsi' => 'required',
-        'foto' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048', // adjust file types and size as needed
+        'foto' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048', 
     ]);
 
-    // Retrieve the currently authenticated user
     $user = auth()->user();
 
     if ($user) {
-        // Add the user_id to the attributes
         $attributes['user_id'] = $user->id;
 
-        // Add the tenant_id (you might get it from the user relationship or another source)
-        // Adjust this part based on your application's structure
         $attributes['tenant_id'] = $user->tenant_id;
 
-        // Set the default status or adjust it based on your requirements
         $attributes['status'] = 1;
 
-        // Handle file upload
         $fotoPath = $attributes['foto']->store('foodpics', 'public');
         $attributes['foto'] = $fotoPath;
 
-        // Create the menu
         $menu = Menu::create($attributes);
 
-        // Redirect or return a response
         return redirect()->route('showtenant')->with('success', 'Menu added successfully');
     } else {
-        // Handle the case when no user is authenticated
-        // You might want to redirect to a login page or handle this case as needed
         return redirect()->route('login')->with('error', 'Authentication error');
     }
 }
