70523edf5f871882bc450504a757996bdbbb124e
Gabrielle Antoinette <gabrielle.a21@mhs.istts.ac.id>
2023-12-05 09:01:43 +0700

    revisi tampilan, tampilin user tenant yg login

--- resources/views/layouts/navbars/auth/sidenavTenant.blade.php
+++ resources/views/layouts/navbars/auth/sidenavTenant.blade.php
@@ -81,7 +81,7 @@
                 <h6 class="ps-4 ms-2 text-uppercase text-xs font-weight-bolder opacity-6">Account Tenant</h6>
             </li>
             <li class="nav-item">
-                <a class="nav-link {{ Route::currentRouteName() == 'profile-static' ? 'active' : '' }}" href="{{ route('profile-static') }}">
+                <a class="nav-link {{ Route::currentRouteName() == 'profileTenant' ? 'active' : '' }}" href="{{ route('profileTenant') }}">
                     <div
                         class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
                         <i class="ni ni-single-02 text-dark text-sm opacity-10"></i>
--- routes/web.php
+++ routes/web.php
@@ -72,6 +72,7 @@ Route::group(['middleware' => 'auth'], function () {
 	Route::get('/profile', [UserProfileController::class, 'show'])->name('profile');
 	Route::post('/profile', [UserProfileController::class, 'update'])->name('profile.update');
 	Route::get('/profile-static', [PageController::class, 'profile'])->name('profile-static');
+	Route::get('/profileTenant', [PageController::class, 'profileTenant'])->name('profileTenant');
 	Route::get('/sign-in-static', [PageController::class, 'signin'])->name('sign-in-static');
 	Route::get('/sign-up-static', [PageController::class, 'signup'])->name('sign-up-static');
 	Route::get('/{page}', [PageController::class, 'index'])->name('page');
