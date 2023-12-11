4ac2dd570b6abebec1adc7c4eef5630acf67ccaf
Gabrielle Antoinette <gabrielle.a21@mhs.istts.ac.id>
2023-12-12 00:01:39 +0700

    bisa delete, ubah kategori jd rb, testing

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Menu;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -28,8 +29,10 @@ class HomeController extends Controller
     }
     public function showtenant()
     {
-        $menus = Menu::all();
+        $activeUser = Auth::user();
+
+        $menus = Menu::where('user_id', $activeUser->user_id)->get();
         return view('pages.hlmnTenant', compact('menus'));
-        // return view('pages.hlmnTenant');
+
     }
 }
