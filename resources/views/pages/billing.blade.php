708ac8d546d0e79ae11c0b3ea4f586db22492f98
lingga24 <lingga.dian.lestari@gmail.com>
2024-01-10 22:30:24 +0700

    chart dashboard

--- resources/views/pages/billing.blade.php
+++ resources/views/pages/billing.blade.php
@@ -4,7 +4,7 @@
     @include('layouts.navbars.auth.topnav', ['title' => 'Report'])
     <div class="container-fluid py-4">
         <div class="row">
-            <div class="col-lg-8">
+            <div class="col-lg-12">
                 <div class="row">
                     <div class="col-xl-12">
                         <div class="row">
@@ -17,10 +17,20 @@
                                         </div>
                                     </div>
                                     <div class="card-body pt-0 p-3 text-center">
+                                        @php
+                                            use App\Models\H_Bulan;
+                                            $currentMonthAngka = date('n');
+                                            $currentMonthHuruf = date('F');
+                                            $currentYear = date('Y');
+                                            $totalPengeluaran = H_Bulan::where('status', 0)
+                                            ->whereMonth('created_at', $currentMonthAngka)
+                                            ->whereYear('created_at', $currentYear)
+                                            ->sum('total');
+                                        @endphp
                                         <h6 class="text-center mb-0">Expenses</h6>
-                                        <span class="text-xs">Pengeluaran per bulan</span>
+                                        <span class="text-xs">for {{ $currentMonthHuruf." ".$currentYear }}</span>
                                         <hr class="horizontal dark my-3">
-                                        <h5 class="mb-0">Rp5.700.000</h5>
+                                        <h5 class="mb-0">Rp{{ number_format($totalPengeluaran, 0, ',', '.') }}</h5>
                                     </div>
                                 </div>
                             </div>
@@ -33,189 +43,24 @@
                                         </div>
                                     </div>
                                     <div class="card-body pt-0 p-3 text-center">
+                                        @php
+                                            use App\Models\H_Tenant;
+                                            use App\Models\H_Kamar;
+                                            $totalPemasukanHTenant = H_Tenant::whereMonth('created_at', $currentMonthAngka)
+                                            ->whereYear('created_at', $currentYear)
+                                            ->sum('total');
+                                            $totalPemasukanHKamar = H_Kamar::whereMonth('created_at', $currentMonthAngka)
+                                            ->whereYear('created_at', $currentYear)
+                                            ->sum('total');
+                                        @endphp
                                         <h6 class="text-center mb-0">Incomes</h6>
-                                        <span class="text-xs">Pemasukan per bulan</span>
+                                        <span class="text-xs">for {{ $currentMonthHuruf." ".$currentYear }}</span>
                                         <hr class="horizontal dark my-3">
-                                        <h5 class="mb-0">Rp9.500.000</h5>
+                                        <h5 class="mb-0">Rp{{ number_format($totalPemasukanHTenant+$totalPemasukanHKamar, 0, ',', '.') }}</h5>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4">
-                <div class="card h-100">
-                    <div class="card-header pb-0 p-3">
-                        <div class="row">
-                            <div class="col-6 d-flex align-items-center">
-                                <h6 class="mb-0">Invoice Expenses</h6>
-                            </div>
-                            <div class="col-6 text-end">
-                                <button class="btn btn-outline-primary btn-sm mb-0">All</button>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="card-body p-3 pb-0">
-                        <ul class="list-group">
-                            <li class="list-group-item border-0 d-flex justify-content-between ps-0 mb-2 border-radius-lg">
-                                <div class="d-flex flex-column">
-                                    <h6 class="mb-1 text-dark font-weight-bold text-sm">19 Nov 23</h6>
-                                    <span class="text-xs">HT001</span>
-                                </div>
-                                <div class="d-flex align-items-center text-sm">
-                                    Rp2.000.000 <br> Pay Salary
-                                </div>
-                            </li>
-                            <li class="list-group-item border-0 d-flex justify-content-between ps-0 mb-2 border-radius-lg">
-                                <div class="d-flex flex-column">
-                                    <h6 class="mb-1 text-dark font-weight-bold text-sm">20 Nov 23</h6>
-                                    <span class="text-xs">HT002</span>
-                                </div>
-                                <div class="d-flex align-items-center text-sm">
-                                    Rp3.000.000 <br> Pay Electricity
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-md-7 mt-4">
-                <div class="card">
-                    <div class="card-header pb-0 px-3">
-                        <h6 class="mb-0">Income Information</h6>
-                    </div>
-                    <div class="card-body pt-4 p-3">
-                        <ul class="list-group">
-                            <li class="list-group-item border-0 d-flex p-4 mb-2 bg-gray-100 border-radius-lg">
-                                <div class="d-flex flex-column">
-                                    <h6 class="mb-3 text-sm">Angeline Valencia</h6>
-                                    <span class="mb-2 text-xs">Total Payment: <span
-                                            class="text-dark font-weight-bold ms-sm-2">Rp700.000</span></span>
-                                    <span class="mb-2 text-xs">Description: <span
-                                            class="text-dark ms-sm-2 font-weight-bold">Sewa Kamar Kos</span></span>
-                                    <span class="mb-2 text-xs">Date: <span
-                                            class="text-dark ms-sm-2 font-weight-bold">18/11/2023</span></span>
-                                </div>
-                            </li>
-                            <li class="list-group-item border-0 d-flex p-4 mb-2 bg-gray-100 border-radius-lg">
-                                <div class="d-flex flex-column">
-                                    <h6 class="mb-3 text-sm">Lingga Dian Lestari</h6>
-                                    <span class="mb-2 text-xs">Total Payment: <span
-                                            class="text-dark font-weight-bold ms-sm-2">Rp500.000</span></span>
-                                    <span class="mb-2 text-xs">Description: <span
-                                            class="text-dark ms-sm-2 font-weight-bold">Sewa Stand</span></span>
-                                    <span class="mb-2 text-xs">Date: <span
-                                            class="text-dark ms-sm-2 font-weight-bold">11/10/2023</span></span>
-                                </div>
-                            </li>
-                            <li class="list-group-item border-0 d-flex p-4 mb-2 bg-gray-100 border-radius-lg">
-                                <div class="d-flex flex-column">
-                                    <h6 class="mb-3 text-sm">Arensa</h6>
-                                    <span class="mb-2 text-xs">Total Payment: <span
-                                            class="text-dark font-weight-bold ms-sm-2">Rp900.000</span></span>
-                                    <span class="mb-2 text-xs">Description: <span
-                                            class="text-dark ms-sm-2 font-weight-bold">Sewa Kamar Kos</span></span>
-                                    <span class="mb-2 text-xs">Date: <span
-                                            class="text-dark ms-sm-2 font-weight-bold">01/10/2023</span></span>
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-5 mt-4">
-                <div class="card h-100 mb-4">
-                    <div class="card-header pb-0 px-3">
-                        <div class="row">
-                            <div class="col-md-6">
-                                <h6 class="mb-0">Your Transaction's</h6>
-                            </div>
-                            <div class="col-md-6 d-flex justify-content-end align-items-center">
-                                <i class="far fa-calendar-alt me-2"></i>
-                                <small>Last Month</small>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="card-body pt-4 p-3">
-                        <h6 class="text-uppercase text-body text-xs font-weight-bolder mb-3">Newest</h6>
-                        <ul class="list-group">
-                            <li class="list-group-item border-0 d-flex justify-content-between ps-0 mb-2 border-radius-lg">
-                                <div class="d-flex align-items-center">
-                                    <button
-                                        class="btn btn-icon-only btn-rounded btn-outline-danger mb-0 me-3 btn-sm d-flex align-items-center justify-content-center"><i
-                                            class="fas fa-arrow-down"></i></button>
-                                    <div class="d-flex flex-column">
-                                        <h6 class="mb-1 text-dark text-sm">Pay Salary</h6>
-                                        <span class="text-xs">19 Nov 2023, at 12:30 PM</span>
-                                    </div>
-                                </div>
-                                <div class="d-flex align-items-center text-danger text-gradient text-sm font-weight-bold">
-                                    Rp2.000.000
-                                </div>
-                            </li>
-                            <li class="list-group-item border-0 d-flex justify-content-between ps-0 mb-2 border-radius-lg">
-                                <div class="d-flex align-items-center">
-                                    <button
-                                        class="btn btn-icon-only btn-rounded btn-outline-danger mb-0 me-3 btn-sm d-flex align-items-center justify-content-center"><i
-                                            class="fas fa-arrow-down"></i></button>
-                                    <div class="d-flex flex-column">
-                                        <h6 class="mb-1 text-dark text-sm">Pay Electricity</h6>
-                                        <span class="text-xs">20 Nov 2023, at 12:30 PM</span>
-                                    </div>
-                                </div>
-                                <div class="d-flex align-items-center text-danger text-gradient text-sm font-weight-bold">
-                                    Rp3.000.000
-                                </div>
-                            </li>
-                        </ul>
-                        <h6 class="text-uppercase text-body text-xs font-weight-bolder my-3">Yesterday</h6>
-                        <ul class="list-group">
-                            <li class="list-group-item border-0 d-flex justify-content-between ps-0 mb-2 border-radius-lg">
-                                <div class="d-flex align-items-center">
-                                    <button
-                                        class="btn btn-icon-only btn-rounded btn-outline-success mb-0 me-3 btn-sm d-flex align-items-center justify-content-center"><i
-                                            class="fas fa-arrow-up"></i></button>
-                                    <div class="d-flex flex-column">
-                                        <h6 class="mb-1 text-dark text-sm">Sewa Kamar Kos</h6>
-                                        <span class="text-xs">1 Oktober 2023, at 13:45 PM</span>
-                                    </div>
-                                </div>
-                                <div class="d-flex align-items-center text-success text-gradient text-sm font-weight-bold">
-                                    Rp900.000
-                                </div>
-                            </li>
-                            <li class="list-group-item border-0 d-flex justify-content-between ps-0 mb-2 border-radius-lg">
-                                <div class="d-flex align-items-center">
-                                    <button
-                                        class="btn btn-icon-only btn-rounded btn-outline-success mb-0 me-3 btn-sm d-flex align-items-center justify-content-center"><i
-                                            class="fas fa-arrow-up"></i></button>
-                                    <div class="d-flex flex-column">
-                                        <h6 class="mb-1 text-dark text-sm">Sewa Stand</h6>
-                                        <span class="text-xs">11 Oktober 2023, at 12:30 PM</span>
-                                    </div>
-                                </div>
-                                <div class="d-flex align-items-center text-success text-gradient text-sm font-weight-bold">
-                                    Rp500.000
-                                </div>
-                            </li>
-                            <li class="list-group-item border-0 d-flex justify-content-between ps-0 mb-2 border-radius-lg">
-                                <div class="d-flex align-items-center">
-                                    <button
-                                        class="btn btn-icon-only btn-rounded btn-outline-success mb-0 me-3 btn-sm d-flex align-items-center justify-content-center"><i
-                                            class="fas fa-arrow-up"></i></button>
-                                    <div class="d-flex flex-column">
-                                        <h6 class="mb-1 text-dark text-sm">Sewa Kamar Kos</h6>
-                                        <span class="text-xs">18 November 2023, at 08:30 AM</span>
-                                    </div>
-                                </div>
-                                <div class="d-flex align-items-center text-success text-gradient text-sm font-weight-bold">
-                                    Rp700.000
-                                </div>
-                            </li>
-                        </ul>
                     </div>
                 </div>
             </div>
